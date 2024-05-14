9f84464d5b195f204d97833368d5197d3c0f0f76
DevShag <71402521+DevShag@users.noreply.github.com>
2024-05-14 11:32:46 +0530

    Chain Responsibility Design Pattern
    Chain Responsibility Design Pattern with Logger Example

---
+++ src/chainResponsibility_Logger/ErrorLogProcessor.java
@@ -0,0 +1,19 @@
+package chainResponsibility_Logger;
+
+public class ErrorLogProcessor extends LogProcessor{
+
+	public ErrorLogProcessor(LogProcessor logProcessor) {
+		super(logProcessor);		
+	}
+	
+	public void log(int logLevel, String message) {
+		
+		if(logLevel==ERROR) {
+			System.out.println("ERROR: "+message);
+		}
+		else {
+			super.log(logLevel, message);
+		}
+	}
+
+}
---
+++ src/chainResponsibility_Logger/InfoLogProcessor.java
@@ -0,0 +1,18 @@
+package chainResponsibility_Logger;
+
+public class InfoLogProcessor extends LogProcessor{
+
+	public InfoLogProcessor(LogProcessor logProcessor) {
+		super(logProcessor);		
+	}
+	
+	public void log(int logLevel, String message) {
+		
+		if(logLevel==INFO) {
+			System.out.println("INFO: "+message);
+		}
+		else {
+			super.log(logLevel, message);
+		}
+	}
+}
